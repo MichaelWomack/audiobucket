e60da39f07eebd5e3de1e2efb9cd451940ff55f9
Michael Womack <mwomack93@gmail.com>
2017-01-22 22:28:15 -0500

    checking for localhost to set static resources

--- src/main/java/com/mhw/audiobucket/app/Main.java
+++ src/main/java/com/mhw/audiobucket/app/Main.java
@@ -4,7 +4,10 @@ import com.mhw.audiobucket.app.filters.AuthorizedRequestInterceptor;
 import com.mhw.audiobucket.controllers.ArtistsController;
 import com.mhw.audiobucket.controllers.UsersController;
 import com.mhw.audiobucket.util.Util;
+import spark.Spark;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,13 +22,20 @@ public class Main {
     public static final Logger LOGGER = Logger.getLogger(Main.class.getName());
     public static final String STATIC_RESOURCES = "src/main/resources/public";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnknownHostException {
 
         port(8080);
-        staticFiles.externalLocation(STATIC_RESOURCES);
+        boolean localhost = InetAddress.getLocalHost().getHostName().contains("MacBook-Pro.local");
+        if (localhost) {
+            staticFiles.externalLocation(STATIC_RESOURCES);
+        } else {
+            staticFiles.location("/public");
+        }
 
         before("/api/*", new AuthorizedRequestInterceptor());
-        after("*", (req, res) -> { LOGGER.log(Level.INFO, req.requestMethod() + " " + req.pathInfo()); });
+        after("*", (req, res) -> {
+            LOGGER.log(Level.INFO, req.requestMethod() + " " + req.pathInfo());
+        });
 
         UsersController.run();
         ArtistsController.run();
@@ -33,7 +43,9 @@ public class Main {
         /** The key here is the content type is application/json, which
          * keeps it from returning the index.html
          * **/
-        get("*", CONTENT_TYPE, (req, res) -> { return Util.readResouce("public/index.html"); });
+        get("*", CONTENT_TYPE, (req, res) -> {
+            return Util.readResouce("public/index.html");
+        });
 
     }
 }
