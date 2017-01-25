eefe8ec27da74ffc0e8a628966402fd2219d5417
Michael Womack <mwomack93@gmail.com>
2017-01-24 22:48:58 -0500

    built out basic account settings modal

--- src/main/resources/public/js/components/user-toolbar.js
+++ src/main/resources/public/js/components/user-toolbar.js
@@ -1,12 +1,40 @@
 /**
  * Created by michaelwomack on 1/23/17.
  */
+
 angular.module('app').component('userToolbar', {
-   
+
     templateUrl: 'html/templates/user-toolbar.html',
-    controller: function(Authentication) {
+    controller: function (Authentication, $mdDialog) {
         this.logout = () => {
             Authentication.logout();
         };
+
+        this.openAccountSettings = () => {
+            let accountSettingsDialog = {
+                controller: function () {
+                    this.save = () => {
+                        alert("saved");
+                    };
+
+                    this.close = () => {
+                        $mdDialog.hide();
+                    }
+                    
+                },
+                controllerAs: '$ctrl',
+                templateUrl: 'html/templates/account-settings-dialog.html',
+                parent: angular.element(document.body),
+                clickOutsideToClose: true
+            };
+            $mdDialog.show(accountSettingsDialog);
+        };
+        
+        this.openEditProfile = () => {
+            
+        }
     }
+
 });
+
+
