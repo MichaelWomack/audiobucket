bc7cd1784813ea0ee03a535c59184f2f20a4beb1
Michael Womack <mwomack93@gmail.com>
2017-02-19 13:22:21 -0500

    updating how track objects handled in controllers and view.

--- src/main/resources/public/js/components/tracks-tab-content/tracks-tab-content.js
+++ src/main/resources/public/js/components/tracks-tab-content/tracks-tab-content.js
@@ -5,15 +5,22 @@
 angular.module('app').component('tracksTabContent', {
     templateUrl: 'js/components/tracks-tab-content/tracks-tab-content.html',
     controller: function () {
-        
+
         //Request tracks content from db
-        
+
         this.showTrackForm = false;
-        
+        this.tracks = [{
+            'url': 'assets/bolero.mp3',
+            'type': 'audio/mp3',
+            'name': 'Bolero',
+            'album': 'El Miguel',
+            'artist': 'Don Miguel'
+        }];
+
         this.toggleTrackForm = () => {
             this.showTrackForm = !this.showTrackForm;
         };
-        
-        
+
+
     }
 });
