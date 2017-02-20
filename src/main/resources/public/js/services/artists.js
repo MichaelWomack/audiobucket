782daf8a8e068734fcd6f703ad8b1bbcf48459db
Michael Womack <mwomack93@gmail.com>
2017-02-19 22:26:45 -0500

    building out angular Tracks and Artist services.

--- src/main/resources/public/js/services/artists.js
+++ src/main/resources/public/js/services/artists.js
@@ -1,3 +1,13 @@
 /**
  * Created by michaelwomack on 2/19/17.
  */
+
+angular.module('app').factory('Artists', function($http) {
+    let self = {};
+    
+    self.getById = (artistId) => {
+        return $http.get(`/api/artists/id/${artistId}`);
+    };
+    
+    return self;
+});
--- src/main/resources/public/js/services/tracks.js
+++ src/main/resources/public/js/services/tracks.js
@@ -1,3 +1,16 @@
 /**
  * Created by michaelwomack on 2/19/17.
  */
+angular.module('app').factory('Tracks', function ($http) {
+    let self = {};
+    
+    self.getTrackById = (id) => {
+        return $http.get(`/api/tracks/id/${id}`);
+    };
+    
+    self.getTracksByArtistId = (artistId) => {
+        return $http.get(`/api/tracks/artist_id/${artistId}`); 
+    };
+    
+    return self;
+});
