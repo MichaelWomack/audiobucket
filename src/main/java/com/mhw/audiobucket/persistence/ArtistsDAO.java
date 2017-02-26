c3381e9b90123f6cf58757351384d5328d9779d7
Michael Womack <mwomack93@gmail.com>
2017-02-26 16:29:08 -0500

    updated sql and prepared statements for genre and page_url attributes that were added.

--- src/main/java/com/mhw/audiobucket/persistence/ArtistsDAO.java
+++ src/main/java/com/mhw/audiobucket/persistence/ArtistsDAO.java
@@ -13,8 +13,8 @@ import java.util.List;
 public class ArtistsDAO extends BaseDAO {
 
     private static final String GET_ALL = "select * from Artists";
-    private static final String INSERT = "insert into Artists(name, bio) values(?,?)";
-    private static final String UPDATE = "update Artists set name=?, bio=?";
+    private static final String INSERT = "insert into Artists(name, bio, genre, page_url) values(?,?,?,?)";
+    private static final String UPDATE = "update Artists set name=?, bio=?, genre=?, page_url=?";
     private static final String BY_ID = " where id = ?";
 
     @Override
@@ -36,17 +36,15 @@ public class ArtistsDAO extends BaseDAO {
         }
     }
 
-    //    public Artist getByUserId(long id) throws SQLException, ApplicationConfigException {
-//        try (Connection conn = getConnection()) {
-//            PreparedStatement statement = conn.prepareStatement(GET_ALL + BY_USER_ID);
-//        }
-//    }
+
     public long insert(Artist artist) throws SQLException, ApplicationConfigException {
         try (Connection conn = getConnection()) {
             PreparedStatement statement = conn.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS);
             int col = 1;
             statement.setString(col++, artist.getName());
             statement.setString(col++, artist.getBio());
+            statement.setString(col++, artist.getGenre());
+            statement.setString(col++, artist.getPageUrl());
             statement.execute();
             ResultSet rs = statement.getGeneratedKeys();
 
@@ -64,6 +62,8 @@ public class ArtistsDAO extends BaseDAO {
             int col = 1;
             statement.setString(col++, artist.getName());
             statement.setString(col++, artist.getBio());
+            statement.setString(col++, artist.getGenre());
+            statement.setString(col++, artist.getPageUrl());
             statement.setLong(col++, artist.getId());
             return statement.executeUpdate() == 1;
         }
@@ -74,6 +74,8 @@ public class ArtistsDAO extends BaseDAO {
         artist.setId(rs.getLong("id"));
         artist.setBio(rs.getString("bio"));
         artist.setName(rs.getString("name"));
+        artist.setGenre(rs.getString("genre"));
+        artist.setPageUrl(rs.getString("page_url"));
         return artist;
     }
 
