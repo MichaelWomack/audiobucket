0488db53b5371ccc61c6199beff4d1d3104d8ccc
Michael Womack <mwomack93@gmail.com>
2017-02-27 00:12:40 -0500

    updating PUT for artist to accept file input from client.

--- src/main/java/com/mhw/audiobucket/controllers/ArtistsController.java
+++ src/main/java/com/mhw/audiobucket/controllers/ArtistsController.java
@@ -1,6 +1,7 @@
 package com.mhw.audiobucket.controllers;
 
-import com.google.common.base.Strings;
+import com.google.gson.JsonObject;
+import com.mhw.audiobucket.app.transformer.JsonTransformer;
 import com.mhw.audiobucket.config.exception.ApplicationConfigException;
 import com.mhw.audiobucket.model.Artist;
 import com.mhw.audiobucket.model.Response;
@@ -8,17 +9,20 @@ import com.mhw.audiobucket.model.User;
 import com.mhw.audiobucket.persistence.ArtistsDAO;
 import com.mhw.audiobucket.persistence.UsersDAO;
 import com.mhw.audiobucket.serialization.JsonSerializer;
-import com.mhw.audiobucket.app.transformer.JsonTransformer;
+import com.mhw.audiobucket.util.Util;
+import spark.utils.IOUtils;
 
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static spark.Spark.get;
-import static spark.Spark.post;
-import static spark.Spark.put;
+import static spark.Spark.*;
 
 /**
  * Created by mxw4182 on 12/22/16.
@@ -40,9 +44,25 @@ public class ArtistsController {
 
 
         put("/api/artists", CONTENT_TYPE, (req, res) -> {
-            LOGGER.log(Level.INFO, req.body());
             try {
-                Artist artist = (Artist) JsonSerializer.toObject(req.body(), Artist.class);
+                req.attribute("org.eclipse.jetty.multipartConfig", new MultipartConfigElement("/temp"));
+                HttpServletRequest servletRequest = req.raw();
+                List<Part> parts = (ArrayList) servletRequest.getParts();
+
+                JsonObject requestJson = new JsonObject();
+                for (Part part : parts) {
+                    if (!part.getName().equals("profileImage")) {
+                        requestJson.addProperty(part.getName(), Util.inputStreamToString(part.getInputStream()));
+                    }
+                    else {
+                        try (InputStream profileImageStream = part.getInputStream()) {
+                            //TODO create cloud storage utilities to upload file
+                            LOGGER.log(Level.WARNING, "File included: " + part.getName());
+                        }
+                    }
+                }
+
+                Artist artist = (Artist) JsonSerializer.toObject(requestJson.toString(), Artist.class);
                 if (artist.getId() == 0L) {
                     long artistId = artists.insert(artist);
                     long userId = Long.parseLong(req.cookie("identity"));
@@ -50,17 +70,19 @@ public class ArtistsController {
                     user.setArtistId(artistId);
                     users.update(user);
                     return new Response(true, "Created Artist successfully");
-                }
-                else {
+                } else {
                     boolean updated = artists.update(artist);
                     if (updated) {
                         return new Response(true, "Updated artist info successfully");
-                    }
-                    else {
+                    } else {
                         return new Response(false, "Failed to update artist info.");
                     }
                 }
-            } catch (ApplicationConfigException | SQLException e) {
+            } catch (ApplicationConfigException | SQLException | IOException e) {
+                String message = "Error updating artist info";
+                LOGGER.log(Level.SEVERE, message, e);
+                return new Response(false, message);
+            } catch (Exception e) {
                 String message = "Error updating artist info";
                 LOGGER.log(Level.SEVERE, message, e);
                 return new Response(false, message);
