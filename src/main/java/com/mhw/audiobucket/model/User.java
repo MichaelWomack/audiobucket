7d85ef5d2973ffceec29788db5e923a959ae4e6d
Michael Womack <mwomack93@gmail.com>
2017-03-12 15:56:58 -0400

    added equals() and hashcode()

--- src/main/java/com/mhw/audiobucket/model/User.java
+++ src/main/java/com/mhw/audiobucket/model/User.java
@@ -71,6 +71,32 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != user.id) return false;
+        if (isActive != user.isActive) return false;
+        if (artistId != user.artistId) return false;
+        if (!email.equals(user.email)) return false;
+        if (!password.equals(user.password)) return false;
+        return dateCreated != null ? dateCreated.equals(user.dateCreated) : user.dateCreated == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + email.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
+        result = 31 * result + (isActive ? 1 : 0);
+        result = 31 * result + (int) (artistId ^ (artistId >>> 32));
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
