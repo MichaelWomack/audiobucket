2e4ba0996484ceca36a0d930c8b1fdf1bb5a4836
Michael Womack <mwomack93@gmail.com>
2017-03-12 16:26:32 -0400

    updating test cases for UserDAO

--- src/test/java/com/mhw/audiobucket/persistence/UsersDAOTest.java
+++ src/test/java/com/mhw/audiobucket/persistence/UsersDAOTest.java
@@ -7,9 +7,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by michaelwomack on 3/12/17.
@@ -27,7 +27,7 @@ class UsersDAOTest {
 
     @AfterEach
     void tearDown() throws SQLException, ApplicationConfigException {
-        assertTrue(users.delete(testUser));
+        users.delete(testUser);
     }
 
     @Test
@@ -47,5 +47,37 @@ class UsersDAOTest {
         User resultUser = users.getUserByEmail(testUser.getEmail());
         assertEquals("new_password", resultUser.getPassword());
         assertEquals(id, resultUser.getId());
+    }
+
+    @Test
+    void testGetUserById() throws SQLException, ApplicationConfigException {
+        long id = users.insert(testUser);
+        testUser.setId(id);
+        User resultUser = users.getById(id);
+        assertEquals(testUser.getId(), resultUser.getId());
+        assertEquals(testUser.getEmail(), resultUser.getEmail());
+    }
+
+    @Test
+    void testDeleteUser() throws SQLException, ApplicationConfigException {
+        long id = users.insert(testUser);
+        testUser.setId(id);
+        assertTrue(users.delete(testUser));
+        assertNull(users.getUserByEmail(testUser.getEmail()));
+    }
+
+    @Test
+    void testGetAllUsers() throws SQLException, ApplicationConfigException {
+        long id = users.insert(testUser);
+        List<User> userList = users.getAll();
+        assertNotNull(userList);
+
+        boolean containsTestUser = false;
+        for (User user : userList) {
+            if (user.getEmail().equals(testUser.getEmail()) && user.getId() == id) {
+                containsTestUser = true;
+            }
+        }
+        assertTrue(containsTestUser);
     }
 }
